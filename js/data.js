c9e27e99a73eb01d46ad1af1b636e5c7fb95c259
theyoostink <90541264+theyoostink@users.noreply.github.com>
2026-04-03 17:09:42 -0400

    Placeholder entry

--- js/data.js
+++ js/data.js
@@ -4,12 +4,12 @@ var data = {
 		{
 			src: [""],
 			thumbnail: "",
-			title: "",
-			artist: "",
-			artist_url: "",
-			art_url: "",
-			desc: "",
-			date_str: "",
+			title: "Placeholder Entry",
+			artist: "Placeholder Entry",
+			artist_url: null,
+			art_url: null,
+			desc: "Do not delete this. You can use this to set the order the tags show up in the dropdown.",
+			date_str: "Beginning of Time",
 			tags: [""],
 			hidden: true
 		},
